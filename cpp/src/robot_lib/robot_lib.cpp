87c7c748ea060dff6392a0139e20f97b53f1673a
Orkaboy <orkaboy@gmail.com>
2022-03-24 22:17:25 +0100

    Setting up DXL servo connection

--- cpp/src/robot_lib/robot_lib.cpp
+++ cpp/src/robot_lib/robot_lib.cpp
@@ -1,3 +1,30 @@
 #include "robot_lib.hpp"
+#include <fmt/core.h>
 
+namespace ARC {
 
+constexpr auto PROTOCOL_VERSION = 1.0;
+constexpr auto BAUDRATE = 1000000;
+constexpr auto DEVICENAME = "/dev/ttyUSB0";
+
+RobotArm::RobotArm()
+    : mPortHandler(dynamixel::PortHandler::getPortHandler(DEVICENAME))
+    , mPacketHandler(dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION))
+{
+}
+
+auto RobotArm::Init() -> bool {
+    if(!mPortHandler->openPort()) {
+        fmt::print("Failed to open port {}\n", DEVICENAME);
+        return false;
+    }
+
+    if(!mPortHandler->setBaudRate(BAUDRATE)) {
+        fmt::print("Failed to set baudrate to {}\n", BAUDRATE);
+        return false;
+    }
+
+    return true;
+}
+
+} // namespace ARC
--- cpp/src/robot_lib/robot_lib.hpp
+++ cpp/src/robot_lib/robot_lib.hpp
@@ -2,4 +2,16 @@
 
 #include <dynamixel_sdk/dynamixel_sdk.h>
 
+namespace ARC {
 
+class RobotArm {
+public:
+    RobotArm();
+
+    auto Init() -> bool;
+private:
+    dynamixel::PortHandler *mPortHandler;
+    dynamixel::PacketHandler *mPacketHandler;
+};
+
+} // namespace ARC
