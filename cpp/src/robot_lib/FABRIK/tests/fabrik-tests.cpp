481277462486faafc1243d3ec85bd4132473b0eb
Per Lenander <per.lenander@addiva.se>
2022-05-12 19:29:31 +0200

    constrained FABRIK tests (stubs)

--- cpp/src/robot_lib/FABRIK/tests/fabrik-tests.cpp
+++ cpp/src/robot_lib/FABRIK/tests/fabrik-tests.cpp
@@ -3,6 +3,8 @@
 
 enum class Tests {
     FABRIKLinkConstruct = 0,
+    FABRIKConstruct = 1,
+    FABRIKConstraints = 2,
 };
 
 auto FABRIKLinkConstruct() -> Status {
@@ -34,6 +36,39 @@ auto FABRIKLinkConstruct() -> Status {
     return Status::Ok;
 }
 
+auto FABRIKConstruct() -> Status {
+    {
+        const std::vector<ARC::FABRIK::Link> links = {
+            {{0,0,0}, 3, {0,0,1}},
+            {{0,0,3}, 2, {1,0,0}},
+        };
+        ARC::FABRIK ik(links);
+    }
+    
+    {
+        const std::vector<ARC::FABRIK::Link> links = {
+            {{0,0,0}, 2, {1,0,0}},
+            {{0,2,0}, 3, {0,0,1}},
+            {{3,2,0}, 1, {0,0,1}},
+        };
+        ARC::Real tolerance = 0.00001;
+        uint32_t iterLimit = 10;
+        ARC::FABRIK ik(links, tolerance, iterLimit);
+    }
+
+    return Status::Err;
+}
+
+auto FABRIKConstraints() -> Status {
+    const std::vector<ARC::FABRIK::Link> links = {
+        {{0,0,0}, 3, {0,0,1}},
+        {{0,0,3}, 2, {1,0,0}},
+    };
+    ARC::FABRIK ik(links);
+
+    return Status::Err;
+}
+
 auto main(int argc, char* argv[]) -> int {
     if(argc != 2) {
         return static_cast<int>(Status::Err);
@@ -46,6 +81,10 @@ auto main(int argc, char* argv[]) -> int {
     {
     case Tests::FABRIKLinkConstruct:
         ret = FABRIKLinkConstruct(); break;
+    case Tests::FABRIKConstruct:
+        ret = FABRIKConstruct(); break;
+    case Tests::FABRIKConstraints:
+        ret = FABRIKConstraints(); break;
     default:
         break;
     }
